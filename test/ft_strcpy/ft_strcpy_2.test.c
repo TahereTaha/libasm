23603c26d075d7651024f3e46b24dd1caaef17d0
Taha Tahere <elberkani.taha@gmail.com>
2026-04-28 16:32:49 +0200

    adding a test to check if the finction writes form srcs to dest or not

---
+++ test/ft_strcpy/ft_strcpy_2.test.c
@@ -0,0 +1,29 @@
+
+#include "libasm.h"
+#include <stdio.h>
+#include <string.h>
+
+//	simple hello world test.
+
+#define SRCS_STR "hello world!!!"
+#define DEST_STR "goodbye nothingness..."
+
+int	main(void)
+{
+	char	srcs[40] = SRCS_STR;
+	char	dest[40] = DEST_STR;
+
+	if (!strcmp(srcs, dest))
+		return (1);
+	char	*ret = ft_strcpy(dest, srcs);
+	if (ret != dest)
+		return (1);
+	if (strcmp(srcs, dest))
+		return (1);
+//	printf("dest is: %s\n", dest);
+	if (strcmp(dest, SRCS_STR))
+		return (1);
+	return (0);
+}
+
+
