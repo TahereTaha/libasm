e423e0bf1fc4a290132f405ef07f2f73dc08f479
Taha Tahere <elberkani.taha@gmail.com>
2026-07-01 20:33:54 +0200

    passed the test for storing the error value

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,9 +1,18 @@
 
 section .text
 global ft_write
+extern __errno_location
 
 ft_write:
-	mov eax, 0x1
+	mov rax, 0x1
 	syscall
+	cmp rax, 0
+	jge return
+	mov rbx, rax
+	call __errno_location wrt ..plt
+	neg rbx
+	mov [rax], rbx
+	mov rax, -1
+return:
 	ret
 
