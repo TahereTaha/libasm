b309a8f5046d26d63848edcd6726c2371267c758
Taha Tahere <elberkani.taha@gmail.com>
2026-07-03 17:13:57 +0200

    adding a new teast case

--- test/ft_write/ft_write_4.test.c
+++ test/ft_write/ft_write_4.test.c
@@ -4,15 +4,23 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
 //	forsing the error of EBADF.
 
 int	main(void)
 {
-	ssize_t	my_ret = ft_write(42, "hello", 3);
+	signal(SIGPIPE, SIG_IGN);
+	int	my_pipe[2];
+	pipe(my_pipe);
+	close(my_pipe[0]);
+	ssize_t	my_ret = ft_write(my_pipe[1], "hello", 3);
 	int		my_errno = errno;
 
-	ssize_t	original_ret = write(42, "hello", 3);
+	int	original_pipe[2];
+	pipe(original_pipe);
+	close(original_pipe[0]);
+	ssize_t	original_ret = write(original_pipe[1], "hello", 3);
 	int		original_errno = errno;
 
 	if (my_ret != original_ret)
