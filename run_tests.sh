362968e5c627790d01a42a52662ddca8db082612
Taha Tahere <elberkani.taha@gmail.com>
2026-07-27 22:16:40 +0200

    fixed the script file to report with a exit statuts if any of the scripts failed or not

--- run_tests.sh
+++ run_tests.sh
@@ -7,15 +7,23 @@ i=1
 for test in $tests ; do
 	(
 		if $test ; then
-			echo "\033[32mSuccess\033[0m" at $test
+			echo "\033[32mSuccess\033[0m" at $test ; exit 0
 		else  
-			echo "\033[31mFailed\033[0m" at $test
+			echo "\033[31mFailed\033[0m" at $test ; exit 1
 		fi 
 	) &
 	pids[${i}]=$!
 	i=$(echo $i '+' 1)
 done
+errors=0
 for pid in $pids; do
 	wait $pid
+	if [ $? -ne 0 ]; then
+		errors=1
+	fi
 done
+if [ $errors -ne 0 ]; then 
+	echo "caught an error"
+	exit 1
+fi
 echo 'done with all the tests'
